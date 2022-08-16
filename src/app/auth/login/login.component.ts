37a6ac5991200795ea114eb2b0fd8876684b9431
Arnaud <tsofackarnaud@gmail.com>
2022-08-16 14:39:58 +0400

    Init project, adding material and flexLayout, adding signup and login forms

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+   }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+
+  }
+
+}
